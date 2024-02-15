c7677aa95fdab3db6410e1f4fc65dd22aa134cc4
Taiba-Almujaini <almujaini1995.t@gmail.com>
2024-02-15 12:10:30 +0400

    two strings, a and b, return the result of putting them together in the order abba

---
+++ putting two string togather.java
@@ -0,0 +1,16 @@
+import java.util.Scanner; 
+
+class Main {
+  public static void main(String[] args) {
+    Scanner myObj = new Scanner(System.in);  
+     System.out.println("Enter Your first string :");
+     String s1 = myObj.nextLine();
+     
+     
+     System.out.println("Enter Your second string :");
+    String s2 = myObj.nextLine(); 
+    System.out.println( s1 + s2 +s2 +s1);
+
+  
+  }
+}
